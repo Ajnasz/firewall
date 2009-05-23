4945153cb26bec8710c371526d5175ad0f4e2048
Lajos Koszti <ajnasz@ajnasz.hu>
2009-05-23 11:17:26 +0200

    failsafer installation
    if one of the installation processes are failing then don't countue with
    the next ones

--- install.sh
+++ install.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-sudo cp firewall /etc/init.d/
-sudo cp firewall.conf /etc/
-
-sudo chown root:root /etc/init.d/firewall
-sudo chown root:root /etc/firewall.conf
-
-
-sudo chmod 700 /etc/init.d/firewall
-sudo chmod 600 /etc/firewall.conf
+sudo cp firewall /etc/init.d/ && sudo cp firewall.conf /etc/ && sudo chown root:root /etc/init.d/firewall && sudo chown root:root /etc/firewall.conf && sudo chmod 700 /etc/init.d/firewall && sudo chmod 600 /etc/firewall.conf
