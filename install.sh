b2415243c1fc45f8326b6e939bf00c67be287cdd
Lajos Koszti <ajnasz@ajnasz.hu>
2009-05-23 11:12:37 +0200

    add install sh
    with install sh the firewall will be installed into the
    /etc/init.d/firewall, and the firewall.conf will be installed to the
    /etc/firewall.conf

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo cp firewall /etc/init.d/
+sudo cp firewall.conf /etc/
+
+sudo chown root:root /etc/init.d/firewall
+sudo chown root:root /etc/firewall.conf
+
+
+sudo chmod 700 /etc/init.d/firewall
+sudo chmod 600 /etc/firewall.conf
