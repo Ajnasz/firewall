c9b3ccfc6e41860b4b65c8d81605f541f1c0e259
Lajos Koszti <ajnasz@ajnasz.hu>
2009-06-28 13:50:38 +0200

    ask before overwrite conf file

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-sudo cp firewall /etc/init.d/ && sudo cp firewall.conf /etc/ && sudo chown root:root /etc/init.d/firewall && sudo chown root:root /etc/firewall.conf && sudo chmod 700 /etc/init.d/firewall && sudo chmod 600 /etc/firewall.conf
+sudo cp firewall /etc/init.d/
+if [ -e /etc/firewall.conf ]; then
+  echo 'Configuration file exists, do you want to overwrite it? (y/n)';
+  read overwrite;
+  if [ $overwrite == 'y' ]; then
+    sudo cp firewall.conf /etc/;
+  fi
+fi
+sudo chown root:root /etc/init.d/firewall && sudo chown root:root /etc/firewall.conf && sudo chmod 700 /etc/init.d/firewall && sudo chmod 600 /etc/firewall.conf
